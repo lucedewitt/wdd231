97fae2c1742a5b1c2aba404cdea57209fb41b440
lucedewitt <lucedewitt@byui.edu>
2025-09-19 16:57:42 -0600

    NPS1

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,24 @@
 import { getParkData } from "./parkService.mjs";
 
 const parkData = getParkData();
+
+let disclaimerLink = document.querySelector(".disclaimer a");
+disclaimerLink.innerHTML = parkData.fullName;
+disclaimerLink.setAttribute('href', parkData.url);
+
+let webTitle = document.querySelector("title");
+webTitle.innerHTML = parkData.fullName;
+
+let heroImg = document.querySelector(".hero-banner img");
+heroImg.setAttribute('src', parkData.images[0].url);
+
+let bannerContent = document.querySelector(".hero-banner_content");
+bannerContent.innerHTML = parkInfoTemplate(parkData);
+
+function parkInfoTemplate(info) {
+    return `<a href="#" class="hero-banner_title">${info.name}</a>
+            <p class="hero-banner_subtitle">
+              <span>${info.designation}</span>
+              <span>${info.states}</span>
+            </p>`
+}
